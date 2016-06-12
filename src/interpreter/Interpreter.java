fbde52cea3d28e9f58fa344d05fa1c432ad91d3a
agusbauer <agustinbauer1@gmail.com>
2016-06-12 19:15:56 -0300

    listas en progreso

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -10,17 +10,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import jregex.MatchResult;
+import jregex.Matcher;
 import jregex.Pattern;
 import jregex.Replacer;
-
+import jregex.Substitution;
+import jregex.TextBuffer;
 
 public class Interpreter {
 
-    private static final String RULE_SEPARATOR = "->";
-    private static String TEXT = "TEXT";
-    private static final int RULE_NAME = 0;
+    public static final String RULE_SEPARATOR = "->";
+    public static String TEXT = "TEXT";
+    public static final int RULE_NAME = 0;
     private static final int ORIGINAL_EXPRESSION = 1;
     private static final int REPLACER_EXPRESSION = 2;
     private static final int INITIAL_TOKEN = 0;
@@ -45,18 +50,6 @@ public class Interpreter {
         System.out.println(result);
     }
     
-    /**/
-    private static ArrayList<Rule> getRulesFrom(String rulesText) {
-        String rulesToObtain[] = rulesText.split("\n");
-        ArrayList<Rule> result = new ArrayList<Rule>();
-        for (int i = 0; i < rulesToObtain.length; i++) {
-        	   Rule newRule = textToRule(rulesToObtain[i]);
-        	   System.out.println(newRule);
-        	   result.add(newRule);
-		}
-        return result;
-    }
-    
     private static String fileToString(String fileName) throws FileNotFoundException, IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         String result = "";
@@ -72,13 +65,51 @@ public class Interpreter {
         return result;
     }
     
+    /**/
+    private static ArrayList<Rule> getRulesFrom(String rulesText) {
+        String rulesToObtain[] = rulesText.split("\n");
+        ArrayList<Rule> result = new ArrayList<Rule>();
+        for (int i = 0; i < rulesToObtain.length; i++) {
+        	if(!rulesToObtain[i].isEmpty()){
+	        	Rule newRule = textToRule(rulesToObtain[i]);
+	        	System.out.println(newRule);
+	        	result.add(newRule);
+        	}
+		}
+        return result;
+    }
+    
     private static String translate(String textToTranslate){
         for (int i = 0; i < rules.size(); i++) {
-            Pattern pattern = new Pattern(rules.get(i).getOriginalExpression());
+    		Pattern pattern = new Pattern(rules.get(i).getOriginalExpression());
             Replacer replacer = pattern.replacer(rules.get(i).getReplacerExpression());
             textToTranslate = replacer.replace(textToTranslate);
+        	          
         }
-        return textToTranslate;
+        
+        /* PARSEO DE LISTAS */
+        String spCharWithoutCurrToken =  addEscapeCharacters(SPECIAL_CHARACTERS);
+        spCharWithoutCurrToken = spCharWithoutCurrToken.replace("-", "");
+        Pattern pattern = new Pattern("-"+ "([\\p{Alnum}\\p{Space}"+ spCharWithoutCurrToken +"]*)" + "-");
+        Substitution myOwnModel=new Substitution(){
+			@Override
+			public void appendSubstitution(MatchResult match, TextBuffer tb) {
+				Pattern p = new Pattern("1."+ "([\\p{Graph}\\p{Blank}]*)");
+				Replacer r = p.replacer("<il>"+"$1"+"</il>");
+				String replacedMatch = r.replace(match.toString());
+				p = new Pattern("-"+ "([\\p{Blank}]*)");
+				r = p.replacer("$1");
+				replacedMatch = r.replace(replacedMatch);
+				tb.append("<ol>");
+				tb.append(replacedMatch); 
+				//match.getGroup(MatchResult.MATCH,tb);
+	            tb.append("</ol>");
+			}
+         };
+         Replacer myVeryOwnReplacer=new Replacer(pattern,myOwnModel);
+         textToTranslate = myVeryOwnReplacer.replace(textToTranslate);
+         return textToTranslate;
+         /* FIN PARSEO DE LISTAS */
     }
     
     private static Rule textToRule(String line){
@@ -112,7 +143,7 @@ public class Interpreter {
         else
             result += splittedOriginalExpr[INITIAL_TOKEN] + "([\\p{Graph}\\p{Blank}]*)";
         return result;
-    }
+    }   
     
     private static String replacerExprToRegularExpr(String[] splittedReplacerExpr){
         String result = "";
@@ -122,6 +153,13 @@ public class Interpreter {
 		    result += splittedReplacerExpr[INITIAL_TOKEN] + "$1";
 		 return result;
     }
+    
+    private static List<String> replacerExprToNestedRegExp(String[] splittedReplacerExpr){
+    	List<String> result = new ArrayList<>();
+    	
+    	return result;
+    }
+    
     
     private static String addEscapeCharacters(String expression){
         String result = expression;
